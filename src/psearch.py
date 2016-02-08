Fixed a bug with outlier masking.

src/psearch.py
@@ -75,12 +75,9 @@ class TransitSearch(object):
 
     def __init__(self, infile, inject=False):        
         d = pf.getdata(infile,1)
-        m = (d.mask_ol_1.astype(np.bool) & (d.quality == 0) & isfinite(d.flux_1) 
-             & isfinite(d.time) & isfinite(d.error_1))
-        m = (d.quality == 0) & isfinite(d.flux_1)
-        
-        self.tm = GM(supersampling=10, npol=100, nthr=1)
-        #self.tm = MA(supersampling=12, nthr=1) # Can be switched back when the MA bug is fixed.
+        m = (d.quality == 0) & (~(d.mflags_1 & 2**3).astype(np.bool)) & isfinite(d.flux_1)
+
+        self.tm = MA(supersampling=12, nthr=1) 
         self.em = MA(supersampling=10, nldc=0, nthr=1)
 
         self.epic   = int(basename(infile).split('_')[1])