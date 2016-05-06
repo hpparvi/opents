65ab2f1f79e49990779025bf8226b5863cc716ad
benjaminpope <benjamin.pope@astro.ox.ac.uk>
2016-05-06 12:02:21 +0100

    do remove quality flagged points

--- src/psearch.py
+++ src/psearch.py
@@ -88,7 +88,7 @@ class TransitSearch(object):
     def __init__(self, infile, inject=False, **kwargs):        
         self.d = d = pf.getdata(infile,1)
         m = (d.quality == 0) & (~(d.mflags_1 & 2**3).astype(np.bool)) & isfinite(d.flux_1)
-        m = isfinite(d.flux_1) & (~(d.mflags_1 & 2**3).astype(np.bool))
+        # m = isfinite(d.flux_1) & (~(d.mflags_1 & 2**3).astype(np.bool))
 
         self.Kp = pf.getval(infile,'kepmag')
         self.Kp = self.Kp if not isinstance(self.Kp, Undefined) else nan
