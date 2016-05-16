34294269db774b744d64b60315e506927a277996
benjaminpope <benjamin.pope@astro.ox.ac.uk>
2016-05-16 17:46:32 +0100

    normalize model range correctly

--- src/psearch.py
+++ src/psearch.py
@@ -401,7 +401,7 @@ class TransitSearch(object):
         ax.get_yaxis().get_major_formatter().set_useOffset(False)
         ax.axvline(0, alpha=0.25, ls='--', lw=1)
         [ax.axvline(hd, alpha=0.25, ls='-', lw=1) for hd in hdur]
-        setp(ax, xlim=3*hdur, xlabel='Phase [h]', ylabel='Normalised flux')
+        setp(ax, xlim=3*hdur, ylim=[flux_o.min(),flux_o.max()] xlabel='Phase [h]', ylabel='Normalised flux')
         setp(ax.get_yticklabels(), visible=False)
 
 
