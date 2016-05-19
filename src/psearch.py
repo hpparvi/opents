3e8e7b4526f16dca7369e7581d5adf403fce0218
benjaminpope <benjamin.pope@astro.ox.ac.uk>
2016-05-19 17:14:43 +0100

    two subplots

--- src/psearch.py
+++ src/psearch.py
@@ -319,9 +319,9 @@ class TransitSearch(object):
     @bplot
     def plot_lc_time(self, ax=None):
         # ax.plot(self.time, self.flux_r, lw=1)
-        ax.plot(self.time, self.trend_t, lw=0.5)
         # ax.plot(self.time, self.trend_p+4*(np.percentile(self.flux_r, [99])[0]-1), lw=1)
         ax.plot(self.time, self.flux_r-self.trend_p+np.nanmedian(self.trend_p), '.')
+        ax.plot(self.time, self.trend_t, lw=0.5)
         [ax.axvline(self.bls.tc+i*self._rbls['bls_period'], alpha=0.25, ls='--', lw=1) for i in range(35)]
         setp(ax,xlim=self.time[[0,-1]])#, xlabel='Time', ylabel='Normalised flux')
 
