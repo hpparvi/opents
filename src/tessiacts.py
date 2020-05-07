4f8789d538bc4c02b5e141daaa949f76cfb44ad3
Hannu Parviainen <hannu@iac.es>
2020-05-07 15:20:22 +0100

    Added plotting of the cutout and aperture to the IAC TESS LC report.

--- src/tessiacts.py
+++ src/tessiacts.py
@@ -18,7 +18,14 @@ from pathlib import Path
 from typing import Union, List, Optional, Dict
 
 from astropy.io.fits import Header
-from numpy import median, ndarray, load, concatenate
+from astropy.visualization import simple_norm
+from matplotlib import cm
+from matplotlib.axes import Axes
+from matplotlib.gridspec import GridSpec
+from matplotlib.lines import Line2D
+from matplotlib.pyplot import subplot, setp, figure
+from matplotlib.transforms import offset_copy
+from numpy import median, ndarray, load, concatenate, array
 
 from .tessts import TESSTS
 
@@ -55,6 +62,9 @@ class TESSIACTS(TESSTS):
         except:
             return False
 
+    def set_plot_parameters(self):
+        self._tf_plot_nbins: int = 20
+
     def _reader(self, files: Union[Path, str, List[Path]]):
         if isinstance(files, Path) or isinstance(files, str):
             files = [files]
@@ -72,9 +82,11 @@ class TESSIACTS(TESSTS):
             sector = f['fitsheader'][1]['SECTOR'][1]
             self.sectors.append(sector)
 
-            flux = f['flux_flat']
-            time = f['time_flat'] + self.bjdrefi
-            ferr = f['ferr_flat']
+            m = (f['flux_flat'] > 0.0) & (f['flux_flat'] < 2.0)
+
+            flux = f['flux_flat'][m]
+            time = f['time_flat'][m] + self.bjdrefi
+            ferr = f['ferr_flat'][m]
 
             fraw = f['flux_LC1'] / median(f['flux_LC1'])
             traw = f['time'] + self.bjdrefi
@@ -93,7 +105,141 @@ class TESSIACTS(TESSTS):
         self.flux_raw = concatenate(f2)
         self.flux_detrended = concatenate(fluxes)
         self.mag = self._h0['TESSMAG'][1]
+        self.tic = int(files[0].name.split('_')[1])
 
-        name = self._h0['OBJECT'][1].replace(' ', '_')
+        name = f"TIC_{self.tic:011d}"
         return name, concatenate(times), concatenate(fluxes), concatenate(ferrs)
 
+    def plot_header(self, ax):
+        ax.text(0.01, 0.85, f"{self.name.replace('_', ' ')} - Candidate {self.planet}", size=33, va='top', weight='bold')
+        mags = f"{self.mag:3.1f}" if self.mag else '   '
+        teffs = f"{self.teff:5.0f}" if self.teff else '     '
+        s = (
+            f"$\Delta$BIC {self.dbic:6.1f} | SNR {self.bls.snr:6.1f}   | Period    {self.period:5.2f} d | T$_0$ {self.zero_epoch:10.2f} | Depth {self.depth:5.4f} | Duration {24 * self.duration:>4.2f} h\n"
+            f"Mag     {mags} | TEff {teffs} K | LS Period {self.ls.period:5.2f} d | LS FAP {self.ls.fap:5.4f} | Period limits  {self.pmin:5.2f} – {self.pmax:5.2f} d")
+        ax.text(0.01, 0.55, s, va='top', size=15.6, linespacing=2, family='monospace')
+
+    def plot_report(self):
+        def sb(*nargs, **kwargs) -> Axes:
+            return subplot(*nargs, **kwargs)
+
+        lmargin = 0.06
+        rmargin = 0.01
+
+        headheight = 0.11
+        headpad = 0.04
+        footheight = 0.03
+        footpad = 0.04
+
+        fig = figure(figsize=(16, 19))
+        gs = GridSpec(8, 4, figure=fig, wspace=0.4, hspace=0.5,
+                      left=lmargin, right=1 - rmargin, bottom=footheight + footpad, top=1 - headheight - headpad)
+
+        header = fig.add_axes((0, 1 - headheight, 0.8, headheight), frame_on=False, xticks=[], yticks=[])
+        footer = fig.add_axes((0, 0, 1, footheight), frame_on=False, xticks=[], yticks=[])
+
+        # Cutout
+        # ------
+        gs_cutout = GridSpec(1, 2, figure=fig, wspace=0.03, hspace=0.03,
+                             left=0.81, right=1 - rmargin, bottom=1 - headheight, top=1)
+        ax_cutout = subplot(gs_cutout[0])
+        ax_aperture = subplot(gs_cutout[1])
+        d = self.files[0]['pixel_images'][0]
+        n = simple_norm(d, stretch='log')
+        ax_cutout.matshow(d, norm=n, cmap=cm.gray)
+        ax_aperture.matshow(self.files[0]['aperture'], cmap=cm.gray)
+        setp((ax_cutout, ax_aperture), xticks=[], yticks=[])
+
+        # Flux over time
+        # --------------
+        aflux = sb(gs[0:2, :])
+        self.plot_flux_vs_time(aflux)
+
+        # Periodograms
+        # ------------
+        gs_periodograms = gs[2:4, :2].subgridspec(2, 1, hspace=0.05)
+        abls = sb(gs_periodograms[0])
+        als = sb(gs_periodograms[1], sharex=abls)
+
+        self.bls.plot_snr(abls)
+        abls.text(0.02, 1, 'Periodograms', va='center', ha='left', transform=abls.transAxes, size=11,
+                  bbox=dict(facecolor='w'))
+        tra = offset_copy(abls.transAxes, fig=fig, x=-10, y=10, units='points')
+        abls.text(1, 0, 'BLS', va='bottom', ha='right', transform=tra, size=11, bbox=dict(facecolor='w'))
+
+        self.ls.plot_power(als)
+        tra = offset_copy(als.transAxes, fig=fig, x=-10, y=10, units='points')
+        als.text(1, 0, 'Lomb-Scargle', va='bottom', ha='right', transform=tra, size=11, bbox=dict(facecolor='w'))
+
+        # Per-orbit log likelihood difference
+        # -----------------------------------
+        gs_dll = gs[4:6, :2].subgridspec(2, 1, hspace=0.05)
+        adll = sb(gs_dll[0])
+        adllc = sb(gs_dll[1], sharex=adll)
+
+        self.plot_dll(adll)
+        self.plot_cumulative_dll(adllc)
+
+        adll.text(0.02, 1, 'Per-orbit $\Delta$ log likelihood', va='center', transform=adll.transAxes, size=11,
+                  bbox=dict(facecolor='w'))
+        setp(adll.get_xticklabels(), visible=False)
+        setp(adll, xlabel='')
+
+        # Periodic variability
+        # --------------------
+        apvp = sb(gs[6, :2])
+        apvt = sb(gs[6, 2:])
+
+        self.pvar.plot_over_phase(apvp)
+        apvp.text(0.02, 1, 'Periodic variability', va='center', transform=apvp.transAxes, size=11,
+                  bbox=dict(facecolor='w'))
+
+        self.pvar.plot_over_time(apvt)
+        apvt.text(0.02, 1, 'Periodic variability', va='center', transform=apvt.transAxes, size=11,
+                  bbox=dict(facecolor='w'))
+
+        # Transit and orbit
+        # -----------------
+        gs_to = gs[2:5, 2:].subgridspec(2, 1)
+        atr = sb(gs_to[0])
+        aor = sb(gs_to[1], sharey=atr)
+
+        self.tf_all.plot_transit_fit(atr, nbins=self._tf_plot_nbins)
+        self.plot_folded_orbit(aor)
+
+        atr.text(0.02, 1, 'Phase-folded transit', va='center', transform=atr.transAxes, size=11,
+                 bbox=dict(facecolor='w'))
+        aor.text(0.02, 1, 'Phase-folded orbit', va='center', transform=aor.transAxes, size=11, bbox=dict(facecolor='w'))
+
+        # Even ad odd transit fits
+        # ------------------------
+        gs3 = gs[5, 2:].subgridspec(1, 2, wspace=0.05)
+        ato = sb(gs3[0])
+        atm = sb(gs3[1], sharey=ato)
+
+        self.plot_even_odd((ato, atm))
+        ato.text(0.02, 1, 'Even and odd transits', va='center', transform=ato.transAxes, size=11,
+                 bbox=dict(facecolor='w'))
+        setp(atm.get_yticklabels(), visible=False)
+
+        # Parameter posteriors
+        # --------------------
+        gs_posteriors = gs[7, :].subgridspec(3, 5, hspace=0, wspace=0.01)
+        aposteriors = array([[subplot(gs_posteriors[j, i]) for i in range(5)] for j in range(3)])
+        self.plot_posteriors(aposteriors)
+        aposteriors[0, 0].text(0.02, 1, 'Parameter posteriors', va='center', transform=aposteriors[0, 0].transAxes, size=11,
+                               bbox=dict(facecolor='w'))
+
+        fig.lines.extend([
+            Line2D([0, 1], [1, 1], lw=10, transform=fig.transFigure, figure=fig),
+            Line2D([0, 1], [1 - headheight, 1 - headheight], lw=10, transform=fig.transFigure, figure=fig),
+            Line2D([0, 1], [footheight, footheight], lw=5, transform=fig.transFigure, figure=fig),
+            Line2D([0, 1], [0, 0], lw=10, transform=fig.transFigure, figure=fig)
+        ])
+
+        footer.text(0.5, 0.5, 'Open Exoplanet Transit Search Pipeline', ha='center', va='center', size='large')
+        self.plot_header(header)
+
+        fig.align_ylabels([aflux, abls, als, adll, adllc, apvp])
+        fig.align_ylabels([atr, aor, ato, apvt])
+        return fig
